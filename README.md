e09ed9721451229ff7a3115f90c2a78fc8e2bf52
NikiforovAll <nikiforovalekcey@gmail.com>
2020-05-01 00:13:24 +0300

    add: read from xlsx file and map to entry model

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # edu-scope-to-mindmaps
 
 Input:
-![payload_example](./payload_example.png)
+![payload_example](./assets/payload_example.png)
 
 ```bash
-$ dotnet run --path ./logs --name test.xmind --source-path logs/test.xlsx
+dotnet run --path ./logs --name test.xmind --source-path logs/input.xlsx
 ```
 
 Output:
--- XMindCLI/CommandLine/CreateMindMapCommand.cs
+++ XMindCLI/CommandLine/CreateMindMapCommand.cs
@@ -1,4 +1,8 @@
-﻿using System.IO;
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using OfficeOpenXml;
 using Serilog;
 using XMindAPI;
 namespace XMindCLI.CommandLine
@@ -23,9 +27,73 @@ namespace XMindCLI.CommandLine
             var rootTopic = book
                 .GetPrimarySheet()
                 .GetRootTopic();
-
+            ReadPayload(options.SourcePath);
             rootTopic.Add(book.CreateTopic("Child"));
             book.Save().GetAwaiter().GetResult();
+        }
+
+        private void ReadPayload(string path)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using var package = new ExcelPackage(new FileInfo(path));
+            var sheet = package.Workbook.Worksheets[0];
+            var entries = new List<EduScopeEntry>();
+            var cells = sheet.Cells;
+            for (int i = 2; ; i++)
+            {
+                var cellValue = cells[i, 1].Value;
+                if (cellValue is null)
+                {
+                    break;
+                }
+                var entry = new EduScopeEntry()
+                {
+                    Name = cells[i, 1].Value?.ToString(),
+                    Progress = int.Parse(cells[i, 4].Value?.ToString()),
+                    Status = cells[i, 5].Value?.ToString(),
+                    Priority = cells[i, 6].Value?.ToString(),
+                    Type = cells[i, 7].Value?.ToString(),
+                    Epic = cells[i, 8].Value?.ToString(),
+                    Link = cells[i, 9].Value?.ToString(),
+                };
+                entries.Add(entry);
+                // logger.Debug($"{entry}");
+            }
+            var groupedEntries = entries.ToLookup(entry => entry.Epic);
+            foreach (var group in groupedEntries)
+            {
+                logger.Debug($"");
+                logger.Debug($"{group.Key}");
+                logger.Debug($"");
+                foreach (var e in group)
+                {
+                    logger.Debug($"\t\t{e}");
+                }
+            }
+            // https://github.com/EPPlusSoftware/EPPlus.Sample.NetCore/blob/master/01-GettingStarted/GettingStartedSample.cs
+        }
+
+        private class EduScopeEntry
+        {
+            public string Name { get; set; }
+
+            [Range(0, 100)]
+            public int Progress { get; set; }
+
+            public string Status { get; set; }
+
+            public string Priority { get; set; }
+
+            public string Type { get; set; }
+
+            public string Epic { get; set; }
+
+            public string Link { get; set; }
+
+            public override string ToString() =>
+                // $"{Name} - {Status} - {Priority} - {Epic}";
+                $"{Name}";
+
         }
     }
 }
